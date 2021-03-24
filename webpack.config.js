e3203cf64f1015c489a02321d783b997722ada90
Danil Melentev <mejleht@yandex.ru>
2021-03-24 13:34:09 +0300

    Исправил ошибки

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCssAssetWebpackPlugin = require('optimize-css-assets-webpack-plugin')
 const TerserWebpackPlugin = require('terser-webpack-plugin')
+const autoprefixer = require('autoprefixer')
 
 const isDev = process.env.NODE_ENV === 'development'
 const isProd = !isDev
@@ -52,7 +53,22 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  autoprefixer({
+                    overrideBrowserslist: ['> 1%']
+                  })
+                ],
+                sourceMap: true
+              }
+            }
+          }]
       },
       {
         test: /\.(png|jpg|svg|gif)$/,
